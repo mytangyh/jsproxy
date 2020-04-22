f5125287561a353aa162bea774e0abfb3bd97488
mytangyh <49863234+mytangyh@users.noreply.github.com>
2020-04-22 11:50:25 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -39,7 +39,7 @@ jsproxy_config({
       lines: {
         // 收费版（高权重）
         //'node-cfworker-2.etherdream.com': 4,
-
+        'tang.1450521339.workers.dev':1,
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
         //'b.007.workers.dev': 1,
@@ -88,9 +88,9 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
-    },
+    //'https://www.pornhub.com/': {
+    //  redir: 'https://php.net/'
+   // },
     'http://haha.com/': {
       content: 'Hello World'
     },
